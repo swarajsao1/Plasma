e609cdf3f33267d60e1078eaa892e79c3108ab2d
Swaraj Sao <swarajsao1@gmail.com>
2026-04-25 09:29:17 +0000

    Modified for extract_g and compute_B(g).

--- resources/psiBfield.py
+++ resources/psiBfield.py
@@ -1,36 +1,46 @@
 import os 
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-file_path  = os.path.join(BASE_DIR, "g_file.in")
-
-from freeqdsk import geqdsk
-
-with open(file_path, 'r') as f:
-    g = geqdsk.read(f)
-
-
-# R and Z Coordinates
-r_coords = g.r_grid[:,0]
-z_coords = g.z_grid[0,:]
-
-psi_norm = (g.psi - g.simagx)/(g.psi.max() - g.psi.min())
-
-
-# Radial and Vertical Field Calculation
 import numpy as np
-dpsi_dR = np.gradient(psi_norm, r_coords, axis = 0)
-dpsi_dZ = np.gradient(psi_norm, z_coords, axis =1)
-
-BR = - (1/r_coords) * dpsi_dZ
-BZ = (1/r_coords) * dpsi_dR
-
-
-# Poloidal Field Calculation
+from freeqdsk import geqdsk
 from scipy.interpolate import interp1d
-psi_grid = np.linspace(g.psi.min(), g.psi.max(), len(g.fpol))
-fpol_interp = interp1d(psi_grid, g.fpol)
 
-Bphi = fpol_interp(g.psi)/r_coords
 
-print("g_file keywords extracted, BR Bphi BZ calculated.")
+def extract_g(file_name="g_file.in"):
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(BASE_DIR, file_name)
+
+    with open(file_path, 'r') as f:
+        g = geqdsk.read(f)
+
+    print("g_file keywords extracted")
+    return g
+
+
+def compute_B(g):
+
+    # Coordinates
+    r_coords = g.r_grid[:, 0]
+    z_coords = g.z_grid[0, :]
+
+    # Normalized psi (for plotting only)
+    psi_norm = (g.psi - g.simagx) / (g.psi.max() - g.psi.min())
+
+    # Gradients
+    dpsi_dR = np.gradient(g.psi, r_coords, axis=0, edge_order=2)
+    dpsi_dZ = np.gradient(g.psi, z_coords, axis=1, edge_order=2)
+
+    # Fields
+    BR = -(1 / r_coords) * dpsi_dZ
+    BZ = (1 / r_coords) * dpsi_dR
+
+    # Toroidal field
+    psi_grid = np.linspace(g.psi.min(), g.psi.max(), len(g.fpol))
+    fpol_interp = interp1d(psi_grid, g.fpol)
+
+    Bphi = fpol_interp(g.psi) / r_coords
+
+    
+
+    print("Psi Normalized and Magnetic field BR Bphi BZ computed.")
+
+    return psi_norm, BR, Bphi, BZ
